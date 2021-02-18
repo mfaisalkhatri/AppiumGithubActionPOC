cdba25b75254f23b974fc75f784efd0f555a3019
mfaisalkhatri <mohammadfaisalkhatri@gmail.com>
2021-02-18 12:22:37 +0530

    Updated server url in test

--- src/test/java/DemoTest.java
+++ src/test/java/DemoTest.java
@@ -30,8 +30,8 @@ public class DemoTest {
         caps.setCapability("deviceName", "Android Emulator");
         caps.setCapability("browserName", "Chrome");
         caps.setCapability("chromedriverExecutable", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
-        //driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"),caps);
-         driver = new AndroidDriver<>(server.getUrl(),caps);
+        driver = new AndroidDriver<>(new URL("http://localhost:4723/wd/hub"),caps);
+        // driver = new AndroidDriver<>(server.getUrl(),caps);
     }
 
     private static void startAppiumServer()   {
